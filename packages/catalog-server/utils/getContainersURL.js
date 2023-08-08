0998ca8dfbee5232a95a5f044a7da96788ef6482
NguyenThanhIt9x <67454865+NguyenThanhIt9x@users.noreply.github.com>
2023-08-08 14:34:04 +0700

    Initial commit

---
+++ packages/catalog-server/utils/getContainersURL.js
@@ -0,0 +1,10 @@
+const getContainersURL = ({
+  hostname = 'http://localhost:3000',
+  appName,
+  version,
+  platform,
+}) => {
+  return `${hostname}/${appName}?platform=${platform}&appVersion=${version}`;
+};
+
+export default getContainersURL;
